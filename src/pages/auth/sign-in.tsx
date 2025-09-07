a7cb9906cc9c44c1877d31c6318558aab3400cfb
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-07 18:21:57 -0300

    Sign-In - New Button

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
@@ -12,27 +14,38 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
-  const { register, handleSubmit, formState: {isSubmitting} } = useForm<SignInForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-       toast.success('An authentication link will be sent to your email.', {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("An authentication link will be sent to your email.", {
         action: {
-          label: 'Reenviar',
+          label: "Reenviar",
           onClick: () => {
-            handleSignIn(data)
+            handleSignIn(data);
           },
         },
-      })
+      });
     } catch (error) {
-      toast.error('Invalid Credentials.')
+      toast.error("Invalid Credentials.");
     }
   }
 
   return (
     <>
+      <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild>
+          <Link to="/sign-up" className="absolute right-8 top-8">
+            New bussines
+          </Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
