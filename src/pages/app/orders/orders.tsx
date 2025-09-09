769e381359233d88c5924eca5915665707c10673
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-09 14:51:56 -0300

    chore: translate "Pedidos" to "Orders"

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,7 +15,7 @@ import {
 export function Orders() {
   return (
     <>
-      <Helmet title="Pedidos" />
+      <Helmet title="Orders" />
 
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
