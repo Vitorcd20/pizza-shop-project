5e4c24e8c17c156a0c552cb572126c0bfe025cba
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-12 20:47:22 -0300

    Pages and Components (UI) - Page 404 (Not Found)

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
 import { Dashboard } from "./pages/app/dashboard/dashboard";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
@@ -29,7 +31,7 @@ export const router = createBrowserRouter([
         path: "/sign-in",
         element: <SignIn />,
       },
-       {
+      {
         path: "/sign-up",
         element: <SignUp />,
       },
