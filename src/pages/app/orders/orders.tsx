87ceeacf6ad3bbd713abba41fe70585644ad66a6
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-25 13:23:31 -0300

    API Connection - Order Listing

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -8,10 +8,17 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { OrderTableFilters } from "./order-table-filters";
-import { OrderTableRow } from "./order-table-row";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
+import { OrderTableRow } from "./order-table-row";
 
 export function Orders() {
+  const {data: result} = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders
+  })
+  
   return (
     <>
       <Helmet title="Orders" />
@@ -36,8 +43,8 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />;
+                {result && result.orders.map(order => {
+                  return <OrderTableRow key={order.orderId} order={order} />
                 })}
               </TableBody>
             </Table>
